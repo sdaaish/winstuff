434e3cc1949b55951a4965a61d409ef5cfeac994
Stig Dahl <stig@charlottendal.net>
2016-09-01 16:09:04 +0200

    Removed space and inserted leading 0 in 1-digit hour.

--- tid.cmd
+++ tid.cmd
@@ -11,17 +11,14 @@ set $Datum=%$Year%-%$Mth%-%$Day%
 
 :: Set time
 set $Timma=%Time:~0,2%
+:: Fix with space and not leading zero, from http://ss64.com/nt/syntax-gettime.html
+:: Strip spaces from timma, if any
+set $Timma=%$Timma: =%
+:: Ensure the hours have a leading zero
+if 1%$Timma% LSS 20 Set $Timma=0%$Timma%
 set $Minut=%Time:~3,2%
 set $Sekund=%Time:~6,2%
 set $Tid=%$Timma%:%$Minut%:%$Sekund%
-
-:: Strip any leading spaces
-Set _hh=%$Timma: =%
-echo %_hh%
-
-:: Ensure the hours have a leading zero
-if 1%_hh% LSS 20 Set _hh=0%_hh%
-echo %_hh%
 
 echo Aktuellt datum: %$Datum% och tid: %$Tid%
 ping -n 2 -w 800 127.0.0.1  >NUL 2>NUL
