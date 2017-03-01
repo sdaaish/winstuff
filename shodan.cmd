9b667f8803dbef69378ac618b6d172af3d7b0f03
Stig Dahl <stig@charlottendal.net>
2017-03-01 16:01:57 +0100

    Changed path

--- shodan.cmd
+++ shodan.cmd
@@ -4,6 +4,7 @@
 :: 20160928/SDAA
 
 setlocal
-set $prog=%LocalAppData%\Programs\Python\Python35\scripts\shodan
+:: set $prog=%LocalAppData%\Programs\Python\Python35\scripts\shodan
+set $prog=C:\Python36\scripts\shodan
 py %$prog% %*
 endlocal
