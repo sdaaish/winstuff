e0c07c6122f3f25c532da7f1a71fff5463f1f0a5
Stig Dahl <Stig@charlottendal.net>
2017-03-08 20:44:56 +0100

    Removed emacs stuff from winstuff. Use other repo:emacs.d instead

--- make.cmd
+++ make.cmd
@@ -7,15 +7,6 @@
 for /f %%i in ('dir /b *.cmd') do (call :subroutine %%i)
 
 :: Copy settings to homedir
-
-:: Try to create symlinks. Won't mostly work cause of permissions.
-:: Create them beforehand is recommended.
-:: mklink .emacs path-to-git-repo\.emacs-file
-:: mklink %AppData%\.emacs path-to-git-repo\.emacs-file
-:: mkdir %AppData%\.emacs.d 2>NUL
-:: rmdir /s /q %UserProfile%\.emacs.d
-:: mklink /d %UserProfile%\.emacs.d AppData\Roaming\.emacs.d
-xcopy /YECIFR .emacs.d %AppData%\.emacs.d
 goto eof
 
 :subroutine
