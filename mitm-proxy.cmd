1a3e44523d5512a7eb472dcffc8c5b0b314727fd
Stig Dahl <stig@charlottendal.net>
2017-10-18 17:29:28 +0200

    Added comments.

--- mitm-proxy.cmd
+++ mitm-proxy.cmd
@@ -4,5 +4,5 @@
 :: # 20160630/SDAA
 
 :: -f means go background
-:: -NnT means xxx
+:: -NnT means Dont execute command, redirects sdin from nul, disable pseudotty.
 ssh -f mitm -L 8080:127.0.0.1:8080 -NnT
