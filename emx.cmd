cba03e97aba3ca1a12093f1607b0047a556ecdc6
Stig Dahl <Stig@charlottendal.net>
2018-01-25 19:09:34 +0100

    Removed quotations from parameter.
    Problem to open files with spaces in path when the option was
    surrounded by parameters.
    Removing them fixes this.
    
    But now it's probably not possible to open more files at once from the
    commandline.
    
    Added more comments.

--- emx.cmd
+++ emx.cmd
@@ -4,8 +4,17 @@
 :: Starts daemon if not already started.
 
 :: 2017-04-06/SDAA
+
+:: Register this with 'ftype txtfile="<path-to-file>\emx.cmd" "%1"' as an administrator.
+:: Now you can open an file with emacsclient directly from explorer.
+
+:: If this script is in the path, you can open a file in emacs with 'emx filename'
+
+:: If you want a second frame, just type 'emx'.
+
+:: Apparently you cannot have quotation marks around the variable %* for this to work.
 if [%1]==[] (
    emacsclientw --quiet --alternate-editor="" --create-frame
    ) else (
-   emacsclientw --quiet --alternate-editor="" "%*"
+   emacsclientw --quiet --alternate-editor="" %*
 )
