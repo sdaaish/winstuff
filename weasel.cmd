cd322d9514a9542ad4ab913124c95764bc6109a5
Stig Dahl <stig@charlottendal.net>
2019-03-08 00:04:52 +0100

    Open files in Windows with wsl-version of emacsclient from Windows

---
+++ weasel.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+:: Alias for starting emacsclient inside of WSL from Windows.
+:: Starts daemon if not already started.
+
+:: 2019-03-07/SDAA
+
+:: Register this with 'ftype txtfile="<path-to-file>\weasel.cmd" "%1"' as an administrator.
+:: Now you can open an file with emacsclient inside WSL directly from explorer.
+
+:: If this script is in the path, you can open a file in emacs with 'weasel filename'
+
+:: If you want a second frame, just type 'weasel'.
+
+:: Apparently you cannot have quotation marks around the variable %* or %~s1 for this to work.
+
+if [%1]==[] (
+   wsl.exe -- emacsclient --quiet --alternate-editor="" --create-frame --display :0.0 --no-wait
+   ) else (
+   echo %1
+   wsl.exe -- emacsclient --quiet --alternate-editor=""  --display :0.0 --no-wait "$(wslpath %1)"
+)
