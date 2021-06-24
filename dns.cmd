77c975a69f0471f06d697a343d0d619cd727d2bf
Stig Dahl <stig@charlottendal.net>
2021-06-24 16:10:38 +0200

    Dont use this any more

--- dns.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Checks the DNS-server address for the client
-rem 2016-06-03/sdaa
-Powershell Get-DnsClientServerAddress -Family IPv4
-Powershell Get-DnsClientServerAddress -Family IPv6
-
